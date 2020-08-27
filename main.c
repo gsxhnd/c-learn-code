a60a84b9c30a465eebe8acb69965d79a72421b7b
gsxhnd <gsxhnd@gmail.com>
2020-08-27 11:01:15 +0800

    feat: add test

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <uv.h>
 
+uv_fs_t open_req;
+
+void on_open(uv_fs_t *req) {
+    if (req->result >= 0) {
+        printf("file fd: %i", req->result);
+    } else {
+        fprintf(stderr, "error open file: %s\n", uv_strerror((int) req->result));
+    }
+}
+
 int main() {
-    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
-    uv_loop_init(loop);
-
-    printf("Now quitting.\n");
-    uv_run(loop, UV_RUN_DEFAULT);
-
-    uv_loop_close(loop);
-    free(loop);
+    uv_fs_open(uv_default_loop(), &open_req, "./test.json", O_RDONLY, 0, on_open);
+    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
     return 0;
 }
